a05ad2d5eb852e1ee7c4d470c2ec7f6e9b9e4a35
Abdo Shajari <abd.shajari@gmail.com>
2016-02-20 01:59:02 +0330

    init commit

---
+++ practice.js
@@ -0,0 +1,25 @@
+//var hello = 'hey dude';
+//console.log(hello);
+//hello = 'for fuck sake im changed'
+//console.log(hello);
+//
+// var boob = 'sup';
+// console.log(boob);
+
+var name = 'abdo';
+var lastName = 'shaj';
+var fullName = name + ' ' + lastName;
+console.log(fullName);
+console.log(fullName.length);
+
+// var name = ['zero', 'one', 'two', 'tree', 'four', 'five'];
+// var digit_name = function(n) {
+//   return name[n];
+// };
+// console.log(digit_name(3));
+
+// var today = new Date();
+// var day = today.getDay();
+// console.log(day);
+// var daylist = ["شنبه","یکشنبه","دوشنبه","سه شنبه ","چهارشنبه","پنجشنبه","جمعه"];
+// console.log(" است." + daylist[day] + " امروز");
